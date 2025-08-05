29ef9e47bf20ca8c7a9051c6546b1279b44eace6
Carniceria Monteros <admin@carniceria.com>
2025-08-04 23:16:24 -0300

    Add real Firebase configuration with user credentials

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
 
 // Configuración de Firebase
-// TODO: Reemplazar con tu configuración real de Firebase
 const firebaseConfig = {
-  apiKey: "tu-api-key",
+  apiKey: "AIzaSyCVn2q5J4zl0U1RL8D6nmaWGdq5WPJ2t9Y",
   authDomain: "carniceria-monteros.firebaseapp.com",
   projectId: "carniceria-monteros",
-  storageBucket: "carniceria-monteros.appspot.com",
-  messagingSenderId: "tu-messaging-sender-id",
-  appId: "tu-app-id"
+  storageBucket: "carniceria-monteros.firebasestorage.app",
+  messagingSenderId: "295246860976",
+  appId: "1:295246860976:web:a529e27f8e382c59daa175",
+  measurementId: "G-8LQFDMWNND"
 };
 
 // Inicializar Firebase
@@ -19,5 +20,6 @@ const app = initializeApp(firebaseConfig);
 // Obtener servicios
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
 
 export default app; 
